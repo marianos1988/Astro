fa60cb5d37db8cdd25637691e698060f234bab3c
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2025-05-12 16:15:28 -0300

    Agregar BtnPrincipalCard

--- project/src/services/services.ts
+++ project/src/services/services.ts
@@ -1,10 +1,9 @@
- 
 
 export const getLaunches = async  () => {
     const res = await fetch("https://api.chucknorris.io/jokes/random");
     const data = await res.json();
 
-    const numbers = [1,2,3,4,5,6]
+    const numbers = [1,2,3,4,5,6] 
 
     
     return {
