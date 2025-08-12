abd554221c657142ddccf56af9e158a285f08d23
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2025-08-12 17:34:49 -0300

    Ajustes responsive footer en Events

--- project/src/services/services.ts
+++ project/src/services/services.ts
@@ -1,9 +1,8 @@
-
 export const getLaunches = async  () => {
     const res = await fetch("https://api.chucknorris.io/jokes/random");
     const data = await res.json();
 
-    const numbers = [1,2,3,4,5,6] 
+    const numbers = [1,2,3,4,5,6]
 
     
     return {
@@ -11,4 +10,3 @@ export const getLaunches = async  () => {
         numbers
     }
 }
-
